2f438a35d82da48099e36dfa03d75198ce9c8cfb
URAMOTO Kazunori <kuprij@mac.com>
2018-06-09 06:55:16 +0900

    IODD 2018 のコンテンツを追加

--- config.rb
+++ config.rb
@@ -54,38 +54,9 @@ page '/*.txt', layout: false
 # Proxy pages
 # https://middlemanapp.com/advanced/dynamic-pages/
 
-# proxy(
-#   '/this-page-has-no-template.html',
-#   '/template-file.html',
-#   locals: {
-#     which_fake_page: 'Rendering a fake page with a local variable'
-#   },
-# )
-
 # Helpers
 # Methods defined in the helpers block are available in templates
 # https://middlemanapp.com/basics/helper-methods/
 
-# helpers do
-#   def some_helper
-#     'Helping'
-#   end
-# end
-
 # Mode-specific configuration
 # https://middlemanapp.com/advanced/configuration/#environment-specific-settings
-configure :build do
-  set :css_dir, 'stylesheets'
-  set :js_dir, 'javascripts'
-  set :images_dir, 'images'
-  set :fonts_dir, 'fonts'
-end
-
-configure :development do
-  set :css_dir, 'stylesheets'
-  set :js_dir, 'javascripts'
-  set :images_dir, 'images'
-  set :fonts_dir, 'fonts'
-
-  activate :livereload
-end
